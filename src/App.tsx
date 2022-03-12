82268ff6b2373ea1e870d7098dbc4ffec6ad69b6
Otavio Cornelio <otaviocos14@gmail.com>
2022-03-11 22:39:17 -0300

    Fix routing problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "views/Login";
-import DragonList from "views/DragonList";
-import DragonDetail from "views/DragonDetail";
-import DragonCreate from "views/DragonCreate";
+import { Outlet } from "react-router-dom";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />}>
-                    <Route path="dragons" element={<DragonList />}>
-                        <Route path=":dragonId" element={<DragonDetail />} />
-                    </Route>
-                    <Route path="create_dragon" element={<DragonCreate />} />
-                    <Route
-                        path="*"
-                        element={
-                            <main>
-                                <p>Here be no dragons!</p>
-                            </main>
-                        }
-                    />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+    return <Outlet />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,41 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme, GlobalStyle } from "theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "views/Login";
+import DragonList from "views/DragonList";
+import DragonDetail from "views/DragonDetail";
+import DragonCreate from "views/DragonCreate";
 
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider theme={defaultTheme}>
             <GlobalStyle />
-            <App />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route index element={<Login />} />
+                        <Route path="dragons" element={<DragonList />}>
+                            <Route
+                                path=":dragonId"
+                                element={<DragonDetail />}
+                            />
+                        </Route>
+                        <Route
+                            path="create_dragon"
+                            element={<DragonCreate />}
+                        />
+                        <Route
+                            path="*"
+                            element={
+                                <main>
+                                    <p>Here be no dragons!</p>
+                                </main>
+                            }
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")

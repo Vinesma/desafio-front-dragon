28131129f059fa8d15b42331668301cebe3ac042
Otavio Cornelio <otaviocos14@gmail.com>
2022-03-11 21:58:22 -0300

    Remove unused css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "views/Login";
 import DragonList from "views/DragonList";
